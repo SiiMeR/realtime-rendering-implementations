cd5ba80907df79c2c62f99f578dc890eced79574
Siim Raudsepp <siimraudsepp@msn.com>
2018-08-20 23:51:46 +0300

    Created base scene and chopper

---
+++ Assets/Transformations/Scripts/Chopper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chopper : MonoBehaviour
+{
+
+	public GameObject rotor;
+	public GameObject[] wings;
+	
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{	
+		rotor.transform.Rotate(Vector3.up, 20f * Time.deltaTime);
+	}
+}
